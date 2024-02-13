cf6f4ded7e0679bf02042f979fa249efab3dfb5f
claymtgy <claymtgy@gmail.com>
2024-02-12 18:47:04 -0700

    Remove unnecessary code

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 A budgeting app based on the envelope system.
 
 The app will utilize Python on the backend, a relational database such as MySQL, as well as React and MUI for the front end. 
+
+## Current Project
+
+### Number Crunching
+Though simple, it's still going to take some time to get the number crunching part of the app done. It's being done first to 
+just get a proof-of-concept in a CLI. The logic will eventually be re-factored into a Flask app, and made into a web-app. 
+
+There are a few steps that any contributors should be aware of:
+
+1. Currently, all logic and math is done just off of the local filesystem. The current implemented files that are created and 
+stored are in the root directory of this repo. They should be under the ./incomes.json and the ./envelopes.json files. These
+files will eventually be removed. When the app gets a bit more fleshed out, we will move to a SQLite database for development
+and then it'll be any SQL-based database that all the information will be stored off of.
+2. The number-crunching part is currently done in the ./number-crunching directory. The logic is still in infancy, and I'm 
+still hashing out exactly how I want to have it done. The logic should follow: See if the incomes and expenses are existing 
+-> run initialization steps if necessary -> present options of all functions, run them as the user requests -> close the app.
+3. Most of the processes of 2 will obviously change as development progresses. The database schema is being worked on currently
+and I have a pretty good idea of what I want to happen there. That will be laid out in a db directory soon.
--- number-processing/main.py
+++ number-processing/main.py
@@ -5,12 +5,19 @@ If you're planning on making commits to this, please name global variables at th
 and functions below that.
 
 TO-DO:
-    - Initialization needs a check
+    - Check to see if initialization is working properly
+    - Re-write the start_files to be able to run when the user selects to reset everything.
+    - Re-write initialization to test to see if the incomes.json and envelopes.json files contain
+        valid entries, not just if they exist
+    - Re-write to-do lists as issues in GitHub
+    - Fix initialization. It isn't checking for the right inputs. This shouldn't be an issue when the app is a web-based
+        thing using a db, but it should make dev work just a little faster
+    - Move initialize function into start file, import it, and then just call it in main.py before the main loop starts
+    - Implement JSON deleting
+    - Test JSON Saving and reading
     - envelope_totals is not returning a value, it's returning a dict. This needs to be changed
     - Rename variables (remaining-total is currently for unallocated income funds, not accounted for by an envelope)
     - Create functionality to edit/delete individual envelopes
-    - Implement default path?
-        (may not be useful/practical when running on a web-server)
     - Ensure everything is a functio that can be called to start
     - Wrap the main loop in a function
     - Restructure files for better organization
@@ -19,7 +26,7 @@ TO-DO:
         - Any time a change is made to Expenses, there needs to be a call to calculate remaining total
         - Expenses details to be in a nested dictionary
         - Expenses need to be categorized from a categories list
-        - Expenses need to be able to be voided. 
+        - Expenses need to be able to be voided.
         - Expenses need to have a unique identifier
         - Expenses need to have an amount tied to them as well.
 
@@ -56,15 +63,12 @@ from num_crunching import get_remaining_total, get_total_incomes, get_env_total
 from json_creation import save_jsons, delete_jsons
 
 # Global variables here
-# Many of these could probably be changed from caps depending on what should change throughout the process. 
 CONTINUE = True
 INCOMES_LIST = {}
 TOTAL_INCOME = 0
 ENVELOPES = {}
 DEFAULT_PATH = ''
 INITIALIZATION = True
-
-# This is not a constant variable, but it is global. 
 envelope_totals = 0
 
 
@@ -76,7 +80,7 @@ HAS_GOTTEN_REMANING_TOTAL = False
 # Sets initialization to true or false,
 def initialize():
     if os.path.isfile('../income.json') and os.path.isfile("../envelopes.json"):
-        print("Initialization skipped, moving on.") 
+        print("Initialization skipped, moving on.")
         INITIALIZATION = False
         with open('../income.json', 'r') as f1:
             data_from_incomes = json.load(f1)
@@ -86,15 +90,17 @@ def initialize():
             data_from_envelopes = json.load(f2)
             ENVELOPES = data_from_envelopes
             f2.close()
+    if INITIALIZATION:
+        init_incomes(INCOMES_LIST)
+        init_envelopes(ENVELOPES)
 
-
-if INITIALIZATION:
-    init_incomes(INCOMES_LIST)
-    init_envelopes(ENVELOPES)
+# Runs initialization logic to see if incomes/envelopes need to be generated
+# Will be deprecated when we move to a web server, so not super crucial. But would help development I think.
+initialize()
 
 # Sets the main loop
 while CONTINUE:
-    selection = int(input('Press 1 to get total income, 2 to get envelope totals, or 3 to get remaining. 4 re-run initialization, 5 will save incomes. 6 will stop the program'))
+    selection = int(input('Press 1 to get total income, 2 to get envelope totals, or 3 to get remaining. 4 re-run initialization, 5 will save incomes. 6 will stop the program\n'))
     if selection == 1:
         print("Getting total income")
         TOTAL_INCOME = get_total_incomes(INCOMES_LIST)
@@ -115,28 +121,3 @@ while CONTINUE:
         print("Exiting")
     else:
         print("whoops, invalid choice")
-
-
-
-TOTAL_INCOME = get_total_incomes(INCOMES_LIST)
-
-# The get env totals is takign in incomes instead of envelopes
-#get_env_total(INCOMES_LIST)
-
-envelope_totals = get_env_total(ENVELOPES)
-
-# This needs to be redone. There is too much happening in the maths side. Each equation should only be written once. We should
-# be calling to retotal each thing instad of doing math more than once. 
-# Any math or equation being done here, or nested in a funciton in num_crunching needs to be its own function in num_crunching
-# right now I'm getting envelope totals too many times.
-#get_remaining_total(ENVELOPES, TOTAL_INCOME)
-'''
-print(f"Your total unallocated funds are: \n{get_remaining_total(ENVELOPES, TOTAL_INCOME)}")
-
-save_or_no = input("Would you like to save your incomes and envelopes? y/n\n")
-
-if save_or_no == "y" or save_or_no == "yes":
-    save_jsons(INCOMES_LIST, ENVELOPES)
-elif save_or_no == "delete":
-    delete_jsons()
-'''
